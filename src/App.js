033ad332558f24ff1211287ee825bb0e230f1d98
Michael Akanji <matscode@gmail.com>
2020-11-21 16:58:29 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import {ReactTabulator} from 'react-tabulator'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const [tableData, setTableData] = useState([])
+
+    const columns = [
+        { title: "Name", field: "name", width: 150 },
+        { title: "Age", field: "age", hozAlign: "left", formatter: "progress" },
+        { title: "Favourite Color", field: "col" },
+        { title: "Date Of Birth", field: "dob", hozAlign: "center" },
+        { title: "Rating", field: "rating", hozAlign: "center", formatter: "star" },
+        { title: "Passed?", field: "passed", hozAlign: "center", formatter: "tickCross" }
+    ];
+
+    setTableData([
+        { id: 1, name: "Oli Bob", age: "12", col: "red", dob: "" },
+        { id: 2, name: "Mary May", age: "1", col: "blue", dob: "14/05/1982" },
+        { id: 3, name: "Christine Lobowski", age: "42", col: "green", dob: "22/05/1982" },
+        { id: 4, name: "Brendon Philips", age: "125", col: "orange", dob: "01/08/1980" },
+        { id: 5, name: "Margret Marmajuke", age: "16", col: "yellow", dob: "31/01/1999" },
+    ]);
+
+    return (
+        <section className="container-fluid">
+            <header className=''>
+                <h1 className='font-weight-bold'>
+                    Tecrum Create
+                </h1>
+            </header>
+
+            <main>
+                <em>List employees here</em>
+
+                <ReactTabulator
+                    data={this.tableData}
+                    columns={this.columns}
+                    tooltips={true}
+                    layout={"fitData"}
+                />
+            </main>
+
+            <footer>
+                <small className='text-muted'>
+                    &copy; Footr
+                </small>
+            </footer>
+        </section>
+    );
 }
 
 export default App;
